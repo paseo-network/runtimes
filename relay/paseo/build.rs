7c02b5bbcb66c6a29c484608d8b91eb90452d479
emmanuelm41 <emmanuelm41@gmail.com>
2024-03-29 13:40:57 -0300

    apply modification to bring runtime back to paseo

--- relay/paseo/build.rs
+++ relay/paseo/build.rs
@@ -1,7 +1,7 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
+// This file is part of Paseo.
 
-// Polkadot is free software: you can redistribute it and/or modify
+// Paseo is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
@@ -12,14 +12,14 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+// along with Paseo.  If not, see <http://www.gnu.org/licenses/>.
 
 use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
-	WasmBuilder::new()
-		.with_current_project()
-		.import_memory()
-		.export_heap_base()
-		.build()
+    WasmBuilder::new()
+        .with_current_project()
+        .import_memory()
+        .export_heap_base()
+        .build()
 }
