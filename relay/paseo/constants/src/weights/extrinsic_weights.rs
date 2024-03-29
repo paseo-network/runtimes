be0d87326224753217634c7f10c0d3a50d52409b
emmanuelm41 <emmanuelm41@gmail.com>
2024-03-29 13:39:39 -0300

    base code from polkadot v1.1.3

--- relay/paseo/constants/src/weights/extrinsic_weights.rs
+++ relay/paseo/constants/src/weights/extrinsic_weights.rs
@@ -1,18 +1,18 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
-// This file is part of Paseo.
+// This file is part of Polkadot.
 
-// Paseo is free software: you can redistribute it and/or modify
+// Polkadot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Paseo is distributed in the hope that it will be useful,
+// Polkadot is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Paseo.  If not, see <http://www.gnu.org/licenses/>.
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
 //! DATE: 2023-06-18 (Y/M/D)
@@ -39,43 +39,43 @@ use sp_core::parameter_types;
 use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
-    /// Time to execute a NO-OP extrinsic, for example `System::remark`.
-    /// Calculated by multiplying the *Average* with `1.0` and adding `0`.
-    ///
-    /// Stats nanoseconds:
-    ///   Min, Max: 125_467, 127_402
-    ///   Average:  126_045
-    ///   Median:   126_039
-    ///   Std-Dev:  310.96
-    ///
-    /// Percentiles nanoseconds:
-    ///   99th: 126_699
-    ///   95th: 126_620
-    ///   75th: 126_207
-    pub const ExtrinsicBaseWeight: Weight =
-        Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(126_045), 0);
+	/// Time to execute a NO-OP extrinsic, for example `System::remark`.
+	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
+	///
+	/// Stats nanoseconds:
+	///   Min, Max: 125_467, 127_402
+	///   Average:  126_045
+	///   Median:   126_039
+	///   Std-Dev:  310.96
+	///
+	/// Percentiles nanoseconds:
+	///   99th: 126_699
+	///   95th: 126_620
+	///   75th: 126_207
+	pub const ExtrinsicBaseWeight: Weight =
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(126_045), 0);
 }
 
 #[cfg(test)]
 mod test_weights {
-    use sp_weights::constants;
+	use sp_weights::constants;
 
-    /// Checks that the weight exists and is sane.
-    // NOTE: If this test fails but you are sure that the generated values are fine,
-    // you can delete it.
-    #[test]
-    fn sane() {
-        let w = super::ExtrinsicBaseWeight::get();
+	/// Checks that the weight exists and is sane.
+	// NOTE: If this test fails but you are sure that the generated values are fine,
+	// you can delete it.
+	#[test]
+	fn sane() {
+		let w = super::ExtrinsicBaseWeight::get();
 
-        // At least 10 µs.
-        assert!(
-            w.ref_time() >= 10u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
-            "Weight should be at least 10 µs."
-        );
-        // At most 1 ms.
-        assert!(
-            w.ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-            "Weight should be at most 1 ms."
-        );
-    }
+		// At least 10 µs.
+		assert!(
+			w.ref_time() >= 10u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
+			"Weight should be at least 10 µs."
+		);
+		// At most 1 ms.
+		assert!(
+			w.ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+			"Weight should be at most 1 ms."
+		);
+	}
 }
