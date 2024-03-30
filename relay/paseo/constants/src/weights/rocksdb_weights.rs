516b8f1afc0de99e4191a3f0c7bb45c9dfd93e33
emmanuelm41 <emmanuelm41@gmail.com>
2024-03-29 21:19:36 -0300

    initial polkadot 1.1.3 code

--- relay/paseo/constants/src/weights/rocksdb_weights.rs
+++ relay/paseo/constants/src/weights/rocksdb_weights.rs
@@ -18,7 +18,7 @@
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
 //! DATE: 2022-03-29 (Y/M/D)
 //!
-//! DATABASE: `RocksDb`, RUNTIME: `Paseo`
+//! DATABASE: `RocksDb`, RUNTIME: `Polkadot`
 //! BLOCK-NUM: `BlockId::Number(9643856)`
 //! SKIP-WRITE: `false`, SKIP-READ: `false`, WARMUPS: `1`
 //! STATE-VERSION: `V0`, STATE-CACHE-SIZE: `0`
@@ -32,77 +32,77 @@
 //   --mul=1.1
 //   --weight-path=runtime/polkadot/constants/src/weights/
 
-/// Storage DB weights for the `Paseo` runtime and `RocksDb`.
+/// Storage DB weights for the `Polkadot` runtime and `RocksDb`.
 pub mod constants {
-    use frame_support::{
-        parameter_types,
-        weights::{constants, RuntimeDbWeight},
-    };
+	use frame_support::{
+		parameter_types,
+		weights::{constants, RuntimeDbWeight},
+	};
 
-    parameter_types! {
-        /// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
-        /// the runtime.
-        pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
-            // Time to read one storage item.
-            // Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-            //
-            // Stats [NS]:
-            //   Min, Max: 5_015, 1_441_022
-            //   Average:  18_635
-            //   Median:   17_795
-            //   Std-Dev:  4829.75
-            //
-            // Percentiles [NS]:
-            //   99th: 32_074
-            //   95th: 26_658
-            //   75th: 19_363
-            read: 20_499 * constants::WEIGHT_REF_TIME_PER_NANOS,
+	parameter_types! {
+		/// By default, Substrate uses `RocksDB`, so this will be the weight used throughout
+		/// the runtime.
+		pub const RocksDbWeight: RuntimeDbWeight = RuntimeDbWeight {
+			// Time to read one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats [NS]:
+			//   Min, Max: 5_015, 1_441_022
+			//   Average:  18_635
+			//   Median:   17_795
+			//   Std-Dev:  4829.75
+			//
+			// Percentiles [NS]:
+			//   99th: 32_074
+			//   95th: 26_658
+			//   75th: 19_363
+			read: 20_499 * constants::WEIGHT_REF_TIME_PER_NANOS,
 
-            // Time to write one storage item.
-            // Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
-            //
-            // Stats [NS]:
-            //   Min, Max: 16_368, 34_500_937
-            //   Average:  75_882
-            //   Median:   74_236
-            //   Std-Dev:  64706.41
-            //
-            // Percentiles [NS]:
-            //   99th: 111_151
-            //   95th: 92_666
-            //   75th: 80_297
-            write: 83_471 * constants::WEIGHT_REF_TIME_PER_NANOS,
-        };
-    }
+			// Time to write one storage item.
+			// Calculated by multiplying the *Average* of all values with `1.1` and adding `0`.
+			//
+			// Stats [NS]:
+			//   Min, Max: 16_368, 34_500_937
+			//   Average:  75_882
+			//   Median:   74_236
+			//   Std-Dev:  64706.41
+			//
+			// Percentiles [NS]:
+			//   99th: 111_151
+			//   95th: 92_666
+			//   75th: 80_297
+			write: 83_471 * constants::WEIGHT_REF_TIME_PER_NANOS,
+		};
+	}
 
-    #[cfg(test)]
-    mod test_db_weights {
-        use super::constants::RocksDbWeight as W;
-        use frame_support::weights::constants;
+	#[cfg(test)]
+	mod test_db_weights {
+		use super::constants::RocksDbWeight as W;
+		use frame_support::weights::constants;
 
-        /// Checks that all weights exist and have sane values.
-        // NOTE: If this test fails but you are sure that the generated values are fine,
-        // you can delete it.
-        #[test]
-        fn bound() {
-            // At least 1 µs.
-            assert!(
-                W::get().reads(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
-                "Read weight should be at least 1 µs."
-            );
-            assert!(
-                W::get().writes(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
-                "Write weight should be at least 1 µs."
-            );
-            // At most 1 ms.
-            assert!(
-                W::get().reads(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-                "Read weight should be at most 1 ms."
-            );
-            assert!(
-                W::get().writes(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
-                "Write weight should be at most 1 ms."
-            );
-        }
-    }
+		/// Checks that all weights exist and have sane values.
+		// NOTE: If this test fails but you are sure that the generated values are fine,
+		// you can delete it.
+		#[test]
+		fn bound() {
+			// At least 1 µs.
+			assert!(
+				W::get().reads(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
+				"Read weight should be at least 1 µs."
+			);
+			assert!(
+				W::get().writes(1).ref_time() >= constants::WEIGHT_REF_TIME_PER_MICROS,
+				"Write weight should be at least 1 µs."
+			);
+			// At most 1 ms.
+			assert!(
+				W::get().reads(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+				"Read weight should be at most 1 ms."
+			);
+			assert!(
+				W::get().writes(1).ref_time() <= constants::WEIGHT_REF_TIME_PER_MILLIS,
+				"Write weight should be at most 1 ms."
+			);
+		}
+	}
 }
