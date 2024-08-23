423a0734da18ae7d2b5afa29b8f3212b90ca4a1a
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2024-08-23 14:48:59 -0300

    Fix key types

--- system-parachains/asset-hub-paseo/tests/tests.rs
+++ system-parachains/asset-hub-paseo/tests/tests.rs
@@ -35,7 +35,7 @@ use asset_test_utils::{
 use codec::{Decode, Encode};
 use frame_support::{assert_ok, traits::fungibles::InspectEnumerable};
 use parachains_common::{
-	AccountId, AssetHubPolkadotAuraId as AuraId, AssetIdForTrustBackedAssets, Balance,
+	AccountId, AuraId, AssetIdForTrustBackedAssets, Balance,
 };
 use parachains_runtimes_test_utils::SlotDurations;
 use sp_consensus_aura::SlotDuration;
@@ -60,7 +60,7 @@ fn collator_session_key(account: [u8; 32]) -> CollatorSessionKey<Runtime> {
 	CollatorSessionKey::new(
 		AccountId::from(account),
 		AccountId::from(account),
-		SessionKeys { aura: AuraId::from(sp_core::ed25519::Public::from_raw(account)) },
+		SessionKeys { aura: AuraId::from(sp_core::sr25519::Public::from_raw(account)) },
 	)
 }
 
@@ -128,7 +128,7 @@ fn test_ed_is_one_hundredth_of_relay() {
 		.with_session_keys(vec![(
 			AccountId::from(ALICE),
 			AccountId::from(ALICE),
-			SessionKeys { aura: AuraId::from(sp_core::ed25519::Public::from_raw(ALICE)) },
+			SessionKeys { aura: AuraId::from(sp_core::sr25519::Public::from_raw(ALICE)) },
 		)])
 		.build()
 		.execute_with(|| {
@@ -147,7 +147,7 @@ fn test_assets_balances_api_works() {
 		.with_session_keys(vec![(
 			AccountId::from(ALICE),
 			AccountId::from(ALICE),
-			SessionKeys { aura: AuraId::from(sp_core::ed25519::Public::from_raw(ALICE)) },
+			SessionKeys { aura: AuraId::from(sp_core::sr25519::Public::from_raw(ALICE)) },
 		)])
 		.build()
 		.execute_with(|| {
