cb417b9e83e8aa02f0420d09e7475f72631d8d5c
Alejandro Martinez <11448715+al3mart@users.noreply.github.com>
2025-08-27 11:02:35 +0200

    chore(ah): bump spec_version

--- system-parachains/asset-hub-paseo/src/lib.rs
+++ system-parachains/asset-hub-paseo/src/lib.rs
@@ -175,7 +175,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: Cow::Borrowed("asset-hub-paseo"),
 	spec_name: Cow::Borrowed("asset-hub-paseo"),
 	authoring_version: 1,
-	spec_version: 1_006_001,
+	spec_version: 1_006_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 15,
