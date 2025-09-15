11b60aeb8f79bec9b73f5c8f713d3d4fc6c25c63
0xPxt <raventosaleix@gmail.com>
2025-09-15 10:14:20 +0200

    upgrade collectives

--- system-parachains/collectives-paseo/src/fellowship/mod.rs
+++ system-parachains/collectives-paseo/src/fellowship/mod.rs
@@ -41,10 +41,10 @@ pub use origins::{
 };
 use pallet_ranked_collective::EnsureOfRank;
 use pallet_xcm::{EnsureXcm, IsVoiceOfBody};
-use paseo_runtime_constants::{currency::GRAND, time::HOURS, xcm::body::FELLOWSHIP_ADMIN_INDEX};
 use polkadot_runtime_common::impls::{
 	LocatableAssetConverter, VersionedLocatableAsset, VersionedLocationConverter,
 };
+use paseo_runtime_constants::{currency::GRAND, time::HOURS, xcm::body::FELLOWSHIP_ADMIN_INDEX};
 use sp_arithmetic::Permill;
 use sp_core::{ConstU128, ConstU32};
 use sp_runtime::traits::{
@@ -210,7 +210,7 @@ impl pallet_core_fellowship::Config<FellowshipCoreInstance> for Runtime {
 	>;
 	type FastPromoteOrigin = EnsureCanFastPromoteTo;
 	type EvidenceSize = ConstU32<65536>;
-	type MaxRank = ConstU32<9>;
+	type MaxRank = ConstU16<9>;
 }
 
 pub type FellowshipSalaryInstance = pallet_salary::Instance1;
