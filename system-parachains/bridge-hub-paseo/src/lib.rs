6c1761f97b102367ebe33e21fec58f2931cee5a4
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-10-28 12:36:03 +0100

    release/v1.9.3

--- system-parachains/bridge-hub-paseo/src/lib.rs
+++ system-parachains/bridge-hub-paseo/src/lib.rs
@@ -211,7 +211,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("bridge-hub-paseo"),
 	impl_name: Cow::Borrowed("bridge-hub-paseo"),
 	authoring_version: 1,
-	spec_version: 1_009_002,
+	spec_version: 1_009_003,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
