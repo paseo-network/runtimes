9009643b71789487a05701d51f3d25e48de200be
gui1117 <guillaume.thiolliere@parity.io>
2025-10-31 11:07:36 +0900

    configure HOLLAR location

--- system-parachains/people-paseo/src/xcm_config.rs
+++ system-parachains/people-paseo/src/xcm_config.rs
@@ -231,8 +231,8 @@ pub struct Hollar;
 impl ContainsPair<Asset, Location> for Hollar {
 	fn contains(asset: &Asset, origin: &Location) -> bool {
 		const HYDRATION_PARA_ID: u32 = 2034;
-		const PALLET_INSTANCE_HOLLAR: u8 = 50; // TODO: find value
-		const GENERAL_INDEX_HOLLAR: u128 = 1; // TODO: find value
+		const PALLET_INSTANCE_HOLLAR: u8 = 51;
+		const GENERAL_INDEX_HOLLAR: u128 = 222;
 
 		// Check if origin is Hydration.
 		let hydration_origin = Location::new(1, Parachain(HYDRATION_PARA_ID));
