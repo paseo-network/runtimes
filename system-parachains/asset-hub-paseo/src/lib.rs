7b252aa054b0399c31e470c72f9c194889d5d15b
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-11-03 16:48:06 +0100

    chore: bump spec versions

--- system-parachains/asset-hub-paseo/src/lib.rs
+++ system-parachains/asset-hub-paseo/src/lib.rs
@@ -183,7 +183,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: Cow::Borrowed("asset-hub-paseo"),
 	spec_name: Cow::Borrowed("asset-hub-paseo"),
 	authoring_version: 1,
-	spec_version: 1_009_003,
+	spec_version: 2_000_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 15,
