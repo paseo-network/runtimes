62eeb945b51d8c2d6077182b00495f91a44bf186
Tomás Senovilla Polo <tspscgs@gmail.com>
2026-01-08 12:17:38 +0100

    chore: Adjust WeightToFee calculation

--- system-parachains/asset-hub-paseo/src/lib.rs
+++ system-parachains/asset-hub-paseo/src/lib.rs
@@ -349,10 +349,10 @@ impl pallet_transaction_payment::Config for Runtime {
 	// The two generic parameters of `BlockRatioFee` define a rational number that defines the
 	// ref_time to fee mapping. The numbers chosen here are exactly the same as the one from the
 	// `WeightToFeePolynomial` that was used before:
-	// - The numerator is `currency::CENTS` = 1_000_000_000_000 / 30 / 100 = 333_333_333
-	// - The denominator is `100 * Balance::from(ExtrinsicBaseWeight::get().ref_time())`
-	//   - which is 100 * 1_000 * 108_157 = 10_815_700_000
-	type WeightToFee = pallet_revive::evm::fees::BlockRatioFee<333333333, 10_815_700_000, Self>;
+	// - The numerator is `currency::CENTS` = 100_000_000
+	// - The denominator is `200 * Balance::from(ExtrinsicBaseWeight::get().ref_time())`
+	//   - which is 200 * 1_000 * 108_157 = 21_631_400_000
+	type WeightToFee = pallet_revive::evm::fees::BlockRatioFee<100_000_000, 21_631_400_000, Self>;
 	type WeightInfo = weights::pallet_transaction_payment::WeightInfo<Self>;
 }
 
