9870db7c3b2a03ea7b8465f2eeb7a1403c9462eb
Alejandro Martinez <11448715+al3mart@users.noreply.github.com>
2026-01-23 10:12:38 +0100

    release v2.0.5 (#322)
    * chore: Complete proxies in BH now that Polkadot includes them
    
    * chore: Hard pressure inflation
    
    * chore: Add cumulus-pallet-weight-reclaim to all system chains
    
    * chore: Reduce treasury times
    
    * fmt
    
    * chore: Bump spec versions
    
    * [bot] Update bridge-hub-paseo weights from benchmarking
    
    * [bot] Update bridge-hub-paseo weights from benchmarking
    
    * [bot] Update people-paseo weights from benchmarking
    
    * [bot] Update coretime-paseo weights from benchmarking
    
    * [bot] Update collectives-paseo weights from benchmarking
    
    * [bot] Update bridge-hub-paseo weights from benchmarking
    
    * [bot] Update people-paseo weights from benchmarking
    
    * [bot] Update coretime-paseo weights from benchmarking
    
    * [bot] Update collectives-paseo weights from benchmarking
    
    * [bot] Update asset-hub-paseo weights from benchmarking
    
    ---------
    
    Co-authored-by: Tomás Senovilla Polo <tspscgs@gmail.com>
    Co-authored-by: Paseo Bot <paseo-bot@paseo.network>

--- system-parachains/people-paseo/src/lib.rs
+++ system-parachains/people-paseo/src/lib.rs
@@ -99,17 +99,20 @@ pub type SignedBlock = generic::SignedBlock<Block>;
 pub type BlockId = generic::BlockId<Block>;
 
 /// The TransactionExtension to the basic transaction logic.
-pub type TxExtension = (
-	frame_system::CheckNonZeroSender<Runtime>,
-	frame_system::CheckSpecVersion<Runtime>,
-	frame_system::CheckTxVersion<Runtime>,
-	frame_system::CheckGenesis<Runtime>,
-	frame_system::CheckEra<Runtime>,
-	frame_system::CheckNonce<Runtime>,
-	frame_system::CheckWeight<Runtime>,
-	pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
-	frame_metadata_hash_extension::CheckMetadataHash<Runtime>,
-);
+pub type TxExtension = cumulus_pallet_weight_reclaim::StorageWeightReclaim<
+	Runtime,
+	(
+		frame_system::CheckNonZeroSender<Runtime>,
+		frame_system::CheckSpecVersion<Runtime>,
+		frame_system::CheckTxVersion<Runtime>,
+		frame_system::CheckGenesis<Runtime>,
+		frame_system::CheckEra<Runtime>,
+		frame_system::CheckNonce<Runtime>,
+		frame_system::CheckWeight<Runtime>,
+		pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
+		frame_metadata_hash_extension::CheckMetadataHash<Runtime>,
+	),
+>;
 
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
@@ -163,7 +166,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("people-paseo"),
 	impl_name: Cow::Borrowed("people-paseo"),
 	authoring_version: 1,
-	spec_version: 2_000_004,
+	spec_version: 2_000_005,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
@@ -266,7 +269,7 @@ impl pallet_transaction_payment::Config for Runtime {
 	type OnChargeTransaction =
 		pallet_transaction_payment::FungibleAdapter<Balances, ResolveTo<StakingPot, Balances>>;
 	type OperationalFeeMultiplier = ConstU8<5>;
-	type WeightToFee = WeightToFee;
+	type WeightToFee = WeightToFee<Self>;
 	type LengthToFee = ConstantMultiplier<Balance, TransactionByteFee>;
 	type FeeMultiplierUpdate = SlowAdjustingFeeUpdate<Self>;
 	type WeightInfo = weights::pallet_transaction_payment::WeightInfo<Self>;
@@ -332,6 +335,10 @@ impl pallet_message_queue::Config for Runtime {
 impl parachain_info::Config for Runtime {}
 
 impl cumulus_pallet_aura_ext::Config for Runtime {}
+
+impl cumulus_pallet_weight_reclaim::Config for Runtime {
+	type WeightInfo = weights::cumulus_pallet_weight_reclaim::WeightInfo<Runtime>;
+}
 
 parameter_types! {
 	// Fellows pluralistic body.
@@ -628,6 +635,7 @@ construct_runtime!(
 		Timestamp: pallet_timestamp = 2,
 		ParachainInfo: parachain_info = 3,
 		MultiBlockMigrations: pallet_migrations = 4,
+		WeightReclaim: cumulus_pallet_weight_reclaim = 5,
 
 		// Monetary stuff.
 		Balances: pallet_balances = 10,
@@ -681,6 +689,7 @@ mod benches {
 		// Cumulus
 		[cumulus_pallet_parachain_system, ParachainSystem]
 		[cumulus_pallet_xcmp_queue, XcmpQueue]
+		[cumulus_pallet_weight_reclaim, WeightReclaim]
 		[pallet_collator_selection, CollatorSelection]
 		// XCM
 		[pallet_xcm, PalletXcmExtrinsicsBenchmark::<Runtime>]
