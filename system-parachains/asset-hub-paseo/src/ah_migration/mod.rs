7fc89376800aa55f2a8a5643508126e46a7d4144
Tomás Senovilla Polo <117524919+tsenovilla@users.noreply.github.com>
2026-01-26 14:26:07 +0100

    feat: Add staking operator proxy type and allow Staking proxy to add/remove a StakingOperator (#323)
    * chore: Add staking operator proxy type

--- system-parachains/asset-hub-paseo/src/ah_migration/mod.rs
+++ system-parachains/asset-hub-paseo/src/ah_migration/mod.rs
@@ -121,6 +121,7 @@ impl TryConvert<RcProxyType, ProxyType> for RcToProxyType {
 			Auction => Ok(ProxyType::Auction),
 			NominationPools => Ok(ProxyType::NominationPools),
 			ParaRegistration => Ok(ProxyType::ParaRegistration),
+			StakingOperator => Ok(ProxyType::StakingOperator),
 		}
 	}
 }
