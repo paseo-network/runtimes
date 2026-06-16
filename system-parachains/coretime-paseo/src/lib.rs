aeaf13998b57a563d5d7d75b8f3c0e64f7d4b7a1
Tomás Senovilla Polo <tspscgs@gmail.com>
2026-06-16 09:46:07 +0200

    v2.3.1

--- system-parachains/coretime-paseo/src/lib.rs
+++ system-parachains/coretime-paseo/src/lib.rs
@@ -167,7 +167,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("coretime-paseo"),
 	impl_name: Cow::Borrowed("coretime-paseo"),
 	authoring_version: 1,
-	spec_version: 2_003_000,
+	spec_version: 2_003_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
